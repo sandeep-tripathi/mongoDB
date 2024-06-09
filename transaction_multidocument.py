720753a0440fb4a10e5acc02b5ef72fa6b3aac52
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2024-06-09 13:28:08 +0200

    Create transaction_multidocument.py

---
+++ transaction_multidocument.py
@@ -0,0 +1,80 @@
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load config from .env file
+load_dotenv()
+MONGODB_URI = os.environ["MONGODB_URI"]
+
+# Connect to MongoDB cluster with MongoClient
+client = MongoClient(MONGODB_URI)
+
+# Step 1: Define the callback that specifies the sequence of operations to perform inside the transactions.
+def callback(
+    session,
+    transfer_id=None,
+    account_id_receiver=None,
+    account_id_sender=None,
+    transfer_amount=None,
+):
+
+    # Get reference to 'accounts' collection
+    accounts_collection = session.client.bank.accounts
+
+    # Get reference to 'transfers' collection
+    transfers_collection = session.client.bank.transfers
+
+    transfer = {
+        "transfer_id": transfer_id,
+        "to_account": account_id_receiver,
+        "from_account": account_id_sender,
+        "amount": {"$numberDecimal": transfer_amount},
+    }
+
+    # Transaction operations
+    # Important: You must pass the session to each operation
+
+    # Update sender account: subtract transfer amount from balance and add transfer ID
+    accounts_collection.update_one(
+        {"account_id": account_id_sender},
+        {
+            "$inc": {"balance": -transfer_amount},
+            "$push": {"transfers_complete": transfer_id},
+        },
+        session=session,
+    )
+
+    # Update receiver account: add transfer amount to balance and add transfer ID
+    accounts_collection.update_one(
+        {"account_id": account_id_receiver},
+        {
+            "$inc": {"balance": transfer_amount},
+            "$push": {"transfers_complete": transfer_id},
+        },
+        session=session,
+    )
+
+    # Add new transfer to 'transfers' collection
+    transfers_collection.insert_one(transfer, session=session)
+
+    print("Transaction successful")
+
+    return
+
+
+def callback_wrapper(s):
+    callback(
+        s,
+        transfer_id="TR218721873",
+        account_id_receiver="MDB343652528",
+        account_id_sender="MDB574189300",
+        transfer_amount=100,
+    )
+
+
+# TODO: Start a client session, then start a transaction.
+with client.start_session() as session:
+    session.with_transaction(callback_wrapper)
+
+client.close()
